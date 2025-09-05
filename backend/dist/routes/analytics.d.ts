2ae8e7f0a37f9fe7e26cbc233308b2b5dfab8528
prathamo28 <prathamesh.mokal28@gmail.com>
2025-09-05 16:34:11 +0530

    remove

--- backend/dist/routes/analytics.d.ts
+++
@@ -1,4 +0,0 @@
-import express from 'express';
-declare const router: express.Router;
-export default router;
-//# sourceMappingURL=analytics.d.ts.map
--- backend/dist/routes/user.d.ts
+++
@@ -1,4 +0,0 @@
-import express from 'express';
-declare const router: express.Router;
-export default router;
-//# sourceMappingURL=user.d.ts.map
