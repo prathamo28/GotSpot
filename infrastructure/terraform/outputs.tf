c18a972a2c72f8371a43fcdd16b351f41cb83382
prathamo28 <prathamesh.mokal28@gmail.com>
2025-09-08 22:45:01 +0530

    Temporarily disable Cloud Run to unblock infrastructure deployment

--- infrastructure/terraform/outputs.tf
+++ infrastructure/terraform/outputs.tf
@@ -1,9 +1,4 @@
 # Outputs
-output "api_url" {
-  description = "GotSpot API URL"
-  value       = google_cloud_run_service.gotspot_api.status[0].url
-}
-
 output "firestore_database_id" {
   description = "Firestore Database ID"
   value       = local.firestore_database_id
@@ -28,3 +23,9 @@ output "region" {
   description = "GCP Region"
   value       = var.region
 }
+
+# Cloud Run outputs temporarily disabled
+# output "api_url" {
+#   description = "GotSpot API URL"
+#   value       = google_cloud_run_service.gotspot_api.status[0].url
+# }
