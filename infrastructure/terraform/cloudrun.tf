9358b6ed56c6bab81f529a74d1a905e92d91b87d
prathamo28 <prathamesh.mokal28@gmail.com>
2025-09-08 22:27:21 +0530

    Simplify Cloud Run: Minimal configuration to avoid deployment issues

--- infrastructure/terraform/cloudrun.tf
+++ infrastructure/terraform/cloudrun.tf
@@ -15,31 +15,10 @@ resource "google_cloud_run_service" "gotspot_api" {
   }
 
   template {
-    metadata {
-      annotations = {
-        "autoscaling.knative.dev/maxScale"      = var.cloud_run_config.max_instances
-        "autoscaling.knative.dev/minScale"      = var.cloud_run_config.min_instances
-        "run.googleapis.com/cpu-throttling"     = "true"
-        "run.googleapis.com/execution-environment" = "gen2"
-      }
-    }
-
     spec {
-      container_concurrency = var.security_config.rate_limit
-      timeout_seconds      = 300
-      service_account_name = local.gotspot_api_email
-
       containers {
-        image = "gcr.io/google-containers/busybox:1.35"
+        image = "gcr.io/cloudrun/hello"
 
-        resources {
-          limits = {
-            cpu    = var.cloud_run_config.cpu
-            memory = var.cloud_run_config.memory
-          }
-        }
-
-        # Health checks removed temporarily to avoid deployment issues
         ports {
           name           = "http1"
           container_port = 8080
